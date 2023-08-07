04f98e1f8111fd94e990ff08d15f0fce91e23f82
Aaron Deril Dsouza <aaronddouza@gmail.com>
2023-08-07 07:40:52 +0530

    Started Working On Functionality

---
+++ index.js
@@ -0,0 +1,16 @@
+var session = document.getElementById("sessionText");
+var timer = document.getElementById("timer");
+var sessionIncrease = document.getElementById("sessionIcrement");
+var sesionDecrease = document.getElementById("sessionDecrement");
+var breakIncrease = document.getElementById("breakIncrement");
+var breakDecrease = document.getElementById("breakDecrement");
+var sessionDuration = document.getElementById("sessionInterval");
+var breakDuration = document.getElementById("breakInterval");
+var start = document.getElementById("start");
+var pause = document.getElementById("pause");
+var stop = document.getElementById("reset");
+
+
+function timer() {
+
+}
